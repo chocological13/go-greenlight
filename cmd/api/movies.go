9d43a0bc9afd096dc67478c44eefd287fe283b79
chocological13 <155338364+chocological13@users.noreply.github.com>
2025-01-17 04:08:22 +0700

    feat: ch3.6 error handling

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -19,7 +19,8 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		// Use the new notFoundResponse() helper.
+		app.notFoundResponse(w, r)
 		return
 	}
 	// Otherwise, interpolate the movie ID in a placeholder response.
@@ -39,7 +40,7 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	// Encode the struct to JSON and send it as the HTTP response.
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
 	if err != nil {
-		app.logger.Error(err.Error())
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		// Use the new serverErrorResponse() helper.
+		app.serverErrorResponse(w, r, err)
 	}
 }
