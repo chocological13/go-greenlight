c6f511bcd59e3621b8aa9fbf4a95621ba25b38b5
chocological13 <155338364+chocological13@users.noreply.github.com>
2025-01-17 01:44:58 +0700

    feat: ch3.3 encoding json in the internal data

---
+++ internal/data/movies.go
@@ -0,0 +1,17 @@
+package data
+
+import (
+	"time"
+)
+
+// Annotate the Movie struct with struct tags to control how the keys appear in the
+// JSON-encoded output.
+type Movie struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"-"` // Use the - directive
+	Title     string    `json:"title"`
+	Year      int32     `json:"year,omitempty"`    // Add the omitempty directive
+	Runtime   int32     `json:"runtime,omitempty"` // Add the omitempty directive
+	Genres    []string  `json:"genres,omitempty"`  // Add the omitempty directive
+	Version   int32     `json:"version"`
+}
