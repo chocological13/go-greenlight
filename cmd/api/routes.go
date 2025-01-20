07cf06c0945008c55a302f34de78df38ebf3ba3f
chocological13 <155338364+chocological13@users.noreply.github.com>
2025-01-21 01:57:39 +0700

    feat: update movie at PUT v1/movies/:id

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -19,12 +19,12 @@ func (app *application) routes() http.Handler {
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
 	// Register the relevant methods, URL patterns and handler functions for our
-	// endpoints using the HandlerFunc() method. Note that http.MethodGet and
-	// http.MethodPost are constants which equate to the strings "GET" and "POST"
-	// respectively.
+	// endpoints using the HandlerFunc() method
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
+	// Add the route for the PUT /v1/movies/:id endpoint.
+	router.HandlerFunc(http.MethodPut, "/v1/movies/:id", app.updateMovieHandler)
 
 	// Wrap the router with the panic recovery middleware.
 	return app.recoverPanic(router)
