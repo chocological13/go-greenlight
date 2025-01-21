5ba089a55af6cf4836195328158719416643d959
chocological13 <155338364+chocological13@users.noreply.github.com>
2025-01-21 13:59:53 +0700

    feat: add helpers to read query string
    readString()
    readInt()
    readCSV()

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -5,10 +5,12 @@ import (
 	"errors"
 	"fmt"
 	"github.com/julienschmidt/httprouter"
+	"greenlight.strwbry.net/internal/validator" // new import
 	"io"
 	"net/http"
+	"net/url"
 	"strconv"
-	"strings" // new import
+	"strings"
 )
 
 type envelope map[string]any
@@ -137,4 +139,52 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any
 	}
 
 	return nil
+}
+
+// The readString() helper returns a string value from the query string, or the provided
+// default value if no matching key could be found.
+func (app *application) readString(qs url.Values, key string, defaultValue string) string {
+	// Extract the value for a given key from the query string
+	// If no key exists, will return ""
+	s := qs.Get(key)
+
+	// If no key exists, return default value
+	if s == "" {
+		return defaultValue
+	}
+
+	return s
+}
+
+// The readCSV() helper reads a string value from the query string and then splits it
+// into a slice on the comma character. If no matching key could be found, it returns
+// the provided default value.
+func (app *application) readCSV(qs url.Values, key string, defaultValues []string) []string {
+	csv := qs.Get(key)
+
+	if csv == "" {
+		return defaultValues
+	}
+
+	return strings.Split(csv, ",")
+}
+
+// The readInt() helper reads a string value from the query string and converts it to an
+// integer before returning. If no matching key could be found it returns the provided
+// default value. If the value couldn't be converted to an integer, then we record an
+// error message in the provided Validator instance.
+func (app *application) readInt(qs url.Values, key string, defaultValue int, v *validator.Validator) int {
+	s := qs.Get(key)
+
+	if s == "" {
+		return defaultValue
+	}
+
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		v.AddError(key, "must be an integer value")
+		return defaultValue
+	}
+
+	return i
 }
