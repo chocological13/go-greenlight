003ba4d79ec56421bac2abb49a1ab83e04345e16
chocological13 <155338364+chocological13@users.noreply.github.com>
2025-01-23 14:59:49 +0700

    feat(users): add user registration handler and endpoint

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -27,6 +27,9 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
 
+	// users endpoint
+	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
+
 	// Wrap the router with the middleware.
 	return app.recoverPanic(app.rateLimit(router))
 }
