25ca667c0071cb1b78883bea2a5f9f514f242a38
chocological13 <155338364+chocological13@users.noreply.github.com>
2025-01-28 04:29:57 +0700

    feat(cors): add dynamic CORS middleware with trusted origins configuration
    - Implemented `enableCORS` middleware to manage CORS headers based on trusted origins.
    - Added `cors.trustedOrigins` configuration using the `-cors-trusted-origins` flag.
    - Updated middleware chain to include CORS handling.
    - Example HTML page demonstrates CORS functionality (optional inclusion).

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,6 +10,7 @@ import (
 	"log"
 	"log/slog"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -43,6 +44,11 @@ type config struct {
 		username string
 		password string
 		sender   string
+	}
+
+	// Add a cors struct and trustedOrigins field with the type []string.
+	cors struct {
+		trustedOrigins []string
 	}
 }
 
@@ -94,7 +100,19 @@ func main() {
 	flag.StringVar(&cfg.smtp.password, "smtp-password", smtpPassword, "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", smtpSender, "SMTP sender")
 
+	// Use the flag.Func() function to process the -cors-trusted-origins command line
+	// flag. In this we use the strings.Fields() function to split the flag value into a
+	// slice based on whitespace characters and assign it to our config struct.
+	// Importantly, if the -cors-trusted-origins flag is not present, contains the empty
+	// string, or contains only whitespace, then strings.Fields() will return an empty
+	// []string slice.
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
+
 	flag.Parse()
+
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
 	// Create connection pool
